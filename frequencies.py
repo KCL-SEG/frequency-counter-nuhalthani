e7eef5cf146cd54daef534f388d63f1d49c4da04
Noha Althani <noha.althani@kcl.ac.uk>
2022-10-13 13:13:14 +0100

    Solved

--- frequencies.py
+++ frequencies.py
@@ -2,6 +2,13 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
-    frequencies = {}
-    # Your code goes here
+    frequencies = dict()
+    tempList = []
+
+    for i in range (len(items)):
+        items[i] = str(items[i])
+
+    for i in items:
+        frequencies[i] = frequencies.get(i, 0) + 1
+
     return frequencies
